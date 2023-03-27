9abe5989799c9cce946d577f5c4b62325073c1f3
Thomas van den Bighelaar <thomas@beardiegames.com>
2023-03-27 17:14:46 +0200

    started on refactoring: splitting the game code from the gpu code

--- src/lib.rs
+++ src/lib.rs
@@ -28,67 +28,168 @@ pub use instances::*;
 // mod resources;
 // pub use resources::*;
 
-use winit::{
+pub use winit::{
     event::*,
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
+    event_loop::{ControlFlow, EventLoop, EventLoopWindowTarget},
+    window::{Window, WindowBuilder, WindowId},
 };
-use state::State;
+use pollster::block_on;
+pub use state::Renderer;
 
 
-pub async fn run() {
+pub trait GfxHandler {
+    fn on_input(&mut self, event: &WindowEvent) -> bool;
+    
+    fn on_update(&mut self, gfx: &mut Gfx);
+    
+    fn on_resize(&mut self, gfx: &mut Gfx);
+}
+
+pub struct GfxContext<T>
+    where T: GfxHandler + 'static 
+{
+    handler: T,
+    gfx: Gfx,
+    renderer: Renderer,
+}
+
+
+pub struct Gfx {
+    pub canvas: Canvas,
+    pub camera: Camera,
+}
+
+impl Gfx {
+    pub async fn new(event_loop: &EventLoop<()>) -> Self {
+        
+        let window = WindowBuilder::new().build(event_loop).unwrap();
+        
+        let canvas = Canvas::new(window).await;
+        
+        let camera = Camera {
+            eye: (0.0, 1.0, 2.0).into(), // position the camera one unit up and 2 units back, +z is out of the screen
+            target: (0.0, 0.0, 0.0).into(), // have it look at the origin
+            up: cgmath::Vector3::unit_y(), // which way is "up"
+            aspect: canvas.aspect_ratio(),
+            fovy: 45.0,
+            znear: 0.1,
+            zfar: 100.0,
+        };
+        
+        Gfx { canvas, camera }
+    }
+}
+
+pub fn run<T> (mut handler: T) 
+where T: GfxHandler + 'static 
+{  
     env_logger::init();
     let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let mut gfx = block_on(Gfx::new(&event_loop));
+    let mut renderer = Renderer::new(&mut gfx);
     
-    let mut state = State::new(window).await;
-
+    let mut ctx = GfxContext { handler, gfx, renderer };
+    
     event_loop.run(move |event, _, control_flow| {
         match event {
-            Event::WindowEvent {
-                ref event,
-                window_id,
-            } if window_id == state.window().id() => if !state.input(event) { // UPDATED!
-                match event {
-                    WindowEvent::CloseRequested
-                    | WindowEvent::KeyboardInput {
-                        input:
-                            KeyboardInput {
-                                state: ElementState::Pressed,
-                                virtual_keycode: Some(VirtualKeyCode::Escape),
-                                ..
-                            },
-                        ..
-                    } => *control_flow = ControlFlow::Exit,
-                    
-                    WindowEvent::Resized(physical_size) => {
-                        state.resize(*physical_size);
-                    }
-                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                        state.resize(**new_inner_size);
-                    }
-                    
-                    _ => {}
+            Event::WindowEvent { ref event, window_id, } => {
+                if let Some(c) = window_event_handler(
+                    &mut ctx,
+                    event, 
+                    &window_id
+                ) {
+                    *control_flow = c;
                 }
             },
-            Event::RedrawRequested(window_id) if window_id == state.window().id() => {
-                state.update();
-                match state.render() {
-                    Ok(_) => {}
-                    // Reconfigure the surface if lost
-                    Err(wgpu::SurfaceError::Lost) => state.reconfigure_surface(),
-                    // The system is out of memory, we should probably quit
-                    Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
-                    // All other errors (Outdated, Timeout) should be resolved by the next frame
-                    Err(e) => eprintln!("{:?}", e),
+            Event::RedrawRequested(window_id) if window_id == ctx.gfx.canvas.window.id() => {
+                update_handler(&mut ctx);
+                
+                if let Some(c) = draw_handler(&mut ctx) {
+                    *control_flow = c;
                 }
             },
             Event::MainEventsCleared => {
                 // RedrawRequested will only trigger once, unless we manually
                 // request it.
-                state.window().request_redraw();
+                ctx.gfx.canvas.window.request_redraw();
             },
             _ => {}
         }
     });
+}
+
+fn window_event_handler<T>(ctx: &mut GfxContext<T>, event: &WindowEvent, id: &WindowId)
+-> Option<ControlFlow> where T: GfxHandler + 'static 
+{
+
+    if *id == ctx.gfx.canvas.window.id() && !ctx.handler.on_input(event) {
+        match event {
+            WindowEvent::CloseRequested
+            | WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput {
+                        state: ElementState::Pressed,
+                        virtual_keycode: Some(VirtualKeyCode::Escape),
+                        ..
+                    },
+                ..
+            } => Some(ControlFlow::Exit),
+            
+            WindowEvent::Resized(physical_size) => {
+                resize_handler(ctx, *physical_size);
+                None
+            },
+            
+            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                resize_handler(ctx, **new_inner_size);
+                None
+            },
+            _ => { None }
+        }
+    } else {
+        None
+    }
+}
+
+fn resize_handler<T> (ctx: &mut GfxContext<T>, new_size: winit::dpi::PhysicalSize<u32>)
+where T: GfxHandler + 'static 
+{
+    
+    if ctx.gfx.canvas.resize(new_size) {
+        ctx.renderer.resize(&mut ctx.gfx, new_size);
+        ctx.handler.on_resize(&mut ctx.gfx);
+    }
+}
+
+fn update_handler<T> (ctx: &mut GfxContext<T>)
+where T: GfxHandler + 'static 
+{
+    
+    ctx.handler.on_update(&mut ctx.gfx);
+    ctx.renderer.update(&mut ctx.gfx);
+}
+
+fn draw_handler<T>(ctx: &mut GfxContext<T>) -> Option<ControlFlow> 
+where T: GfxHandler + 'static 
+{
+    
+    match ctx.renderer.draw(&mut ctx.gfx) {
+        Ok(_) => { None },
+        
+        // Reconfigure the surface if lost
+        Err(wgpu::SurfaceError::Lost) => {
+            ctx.gfx.canvas.reconfigure_surface();
+            None
+        },
+        
+        // The system is out of memory, we should probably quit
+        Err(wgpu::SurfaceError::OutOfMemory) => {
+            Some(ControlFlow::Exit)
+        },
+        // All other errors (Outdated, Timeout) should be resolved by the next frame
+        Err(e) => {
+            eprintln!("{:?}", e);
+            None
+        },
+    }
 }
--- src/state.rs
+++ src/state.rs
@@ -8,10 +8,8 @@ use cgmath::prelude::*;
 
 
 
-
-pub struct State {
-    canvas: Canvas,
-    render_pipeline: wgpu::RenderPipeline,
+pub struct Renderer {
+    pub render_pipeline: wgpu::RenderPipeline,
     
     vertex_buffer: wgpu::Buffer,
     num_vertices: u32,
@@ -20,8 +18,7 @@ pub struct State {
     
     diffuse_bind_group: wgpu::BindGroup,
     diffuse_texture: texture::Texture,
-    
-    camera: Camera,
+
     camera_uniform: CameraUniform,
     camera_buffer: wgpu::Buffer,
     camera_bind_group: wgpu::BindGroup,
@@ -30,67 +27,66 @@ pub struct State {
     instance_buffer: wgpu::Buffer,
     depth_texture: Texture,
     
-    camera_controller: CameraController,
+    //camera_controller: CameraController,
 }
 
-impl State {
+impl Renderer {
     // Creating some of the wgpu types requires async code
-    pub async fn new(window: Window) -> Self {        
-        let canvas = Canvas::new(window).await;
+    pub fn new(gfx: &mut Gfx) -> Self {        
+        //let canvas = Canvas::new(window).await;
             
         // load texture image
         let diffuse_bytes = include_bytes!("../assets/models/GrassTile.png");
-        let diffuse_texture = texture::Texture::from_bytes(&canvas, diffuse_bytes, "../assets/models/GrassTile.png").unwrap(); // CHANGED!
-        let (texture_bind_group_layout, diffuse_bind_group) = texture::create_diffuse_bindgroup(&canvas, &diffuse_texture);
+        let diffuse_texture = texture::Texture::from_bytes(&gfx.canvas, diffuse_bytes, "../assets/models/GrassTile.png").unwrap(); // CHANGED!
+        let (texture_bind_group_layout, diffuse_bind_group) = texture::create_diffuse_bindgroup(&gfx.canvas, &diffuse_texture);
                   
         // setup render camera
-        let camera = Camera {
-            eye: (0.0, 1.0, 2.0).into(), // position the camera one unit up and 2 units back, +z is out of the screen
-            target: (0.0, 0.0, 0.0).into(), // have it look at the origin
-            up: cgmath::Vector3::unit_y(), // which way is "up"
-            aspect: canvas.aspect_ratio(),
-            fovy: 45.0,
-            znear: 0.1,
-            zfar: 100.0,
-        };
+        // let camera = Camera {
+        //     eye: (0.0, 1.0, 2.0).into(), // position the camera one unit up and 2 units back, +z is out of the screen
+        //     target: (0.0, 0.0, 0.0).into(), // have it look at the origin
+        //     up: cgmath::Vector3::unit_y(), // which way is "up"
+        //     aspect: canvas.aspect_ratio(),
+        //     fovy: 45.0,
+        //     znear: 0.1,
+        //     zfar: 100.0,
+        // };
         
         let mut camera_uniform = CameraUniform::new();
-        camera_uniform.update_view_proj(&camera);
+        camera_uniform.update_view_proj(&gfx.camera);
         
-        let camera_buffer = create_camera_buffer(&canvas, camera_uniform);
-        let (camera_bind_group_layout, camera_bind_group) = create_camera_bindgroup(&canvas, &camera_buffer);
+        let camera_buffer = create_camera_buffer(&gfx.canvas, camera_uniform);
+        let (camera_bind_group_layout, camera_bind_group) = create_camera_bindgroup(&gfx.canvas, &camera_buffer);
                   
         // setup vertex and indices buffers
-        let (vertex_buffer, num_vertices) = vertices::new_vertex_buffer(&canvas.device);
-        let (vertex_index_buffer, num_indices) = vertices::new_index_buffer(&canvas.device);
+        let (vertex_buffer, num_vertices) = vertices::new_vertex_buffer(&gfx.canvas.device);
+        let (vertex_index_buffer, num_indices) = vertices::new_index_buffer(&gfx.canvas.device);
         
         // insantiating multiple render objects
         let instances = setup_instance_data();
-        let instance_buffer = create_instance_buffer(&canvas, &instances);
+        let instance_buffer = create_instance_buffer(&gfx.canvas, &instances);
                   
         // render depth buffering
-        let depth_texture = texture::Texture::create_depth_texture(&canvas, "depth_texture");
+        let depth_texture = texture::Texture::create_depth_texture(&gfx.canvas, "depth_texture");
         
                   
         // setup render pipeline
-        let shader = shaders::create_shader(&canvas.device);
+        let shader = shaders::create_shader(&gfx.canvas.device);
         
         let buffer_layouts = &[Vertex::desc(), InstanceRaw::desc()];
         let bindgroup_layouts = &[&texture_bind_group_layout, &camera_bind_group_layout];
         
         let render_pipeline = render_pipeline::create_new(
-            &canvas.device,
+            &gfx.canvas.device,
             &shader,
-            canvas.config.format,
+            gfx.canvas.config.format,
             buffer_layouts,
             bindgroup_layouts,
         );
 
         // controller for camera movement -> This needs to be refactored into user implementation
-        let camera_controller = CameraController::new(0.2);
+        //let camera_controller = CameraController::new(0.2);
         
         Self {
-            canvas,
             render_pipeline,
             
             vertex_buffer,
@@ -101,7 +97,6 @@ impl State {
             diffuse_bind_group,
             diffuse_texture,
             
-            camera,
             camera_uniform,
             camera_buffer,
             camera_bind_group,
@@ -110,39 +105,34 @@ impl State {
             instance_buffer,
             depth_texture,
             
-            camera_controller,
+            //camera_controller,
         }
-    }
-
-    pub fn window(&self) -> &Window {
-        &self.canvas.window
     }
     
-    pub fn reconfigure_surface(&self) {
-        self.canvas.reconfigure_surface();
-    }
+    // pub fn reconfigure_surface(&self, gfx: &mut Gfx) {
+    //     gfx.canvas.reconfigure_surface();
+    // }
 
-    pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-        if self.canvas.resize(new_size) {
-            self.depth_texture = Texture::create_depth_texture(&self.canvas, "depth_texture");  
+    pub fn resize(&mut self, gfx: &mut Gfx, new_size: winit::dpi::PhysicalSize<u32>) {
+        if gfx.canvas.resize(new_size) {
+            self.depth_texture = Texture::create_depth_texture(&gfx.canvas, "depth_texture");  
         }
     }
 
-    pub fn input(&mut self, event: &WindowEvent) -> bool {
-        self.camera_controller.process_events(event)
+    // pub fn input(&mut self, event: &WindowEvent) -> bool {
+    //     false
+    // }
+
+    pub fn update(&mut self, gfx: &mut Gfx) {
+        self.camera_uniform.update_view_proj(&gfx.camera);
+        gfx.canvas.queue.write_buffer(&self.camera_buffer, 0, bytemuck::cast_slice(&[self.camera_uniform]));
     }
 
-    pub fn update(&mut self) {
-        self.camera_controller.update_camera(&mut self.camera);
-        self.camera_uniform.update_view_proj(&self.camera);
-        self.canvas.queue.write_buffer(&self.camera_buffer, 0, bytemuck::cast_slice(&[self.camera_uniform]));
-    }
-
-    pub fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
-        let output = self.canvas.surface.get_current_texture()?;
+    pub fn draw(&mut self, gfx: &mut Gfx) -> Result<(), wgpu::SurfaceError> {
+        let output = gfx.canvas.surface.get_current_texture()?;
         let canvas = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
         
-        let mut encoder = self.canvas.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+        let mut encoder = gfx.canvas.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
             label: Some("Render Encoder"),
         });
         
@@ -185,96 +175,9 @@ impl State {
         }
     
         // submit will accept anything that implements IntoIter
-        self.canvas.queue.submit(std::iter::once(encoder.finish()));
+        gfx.canvas.queue.submit(std::iter::once(encoder.finish()));
         output.present();
     
         Ok(())
-    }
-}
-
-struct CameraController {
-    speed: f32,
-    is_forward_pressed: bool,
-    is_backward_pressed: bool,
-    is_left_pressed: bool,
-    is_right_pressed: bool,
-}
-
-impl CameraController {
-    fn new(speed: f32) -> Self {
-        Self {
-            speed,
-            is_forward_pressed: false,
-            is_backward_pressed: false,
-            is_left_pressed: false,
-            is_right_pressed: false,
-        }
-    }
-
-    fn process_events(&mut self, event: &WindowEvent) -> bool {
-        match event {
-            WindowEvent::KeyboardInput {
-                input: KeyboardInput {
-                    state,
-                    virtual_keycode: Some(keycode),
-                    ..
-                },
-                ..
-            } => {
-                let is_pressed = *state == ElementState::Pressed;
-                match keycode {
-                    VirtualKeyCode::W | VirtualKeyCode::Up => {
-                        self.is_forward_pressed = is_pressed;
-                        true
-                    }
-                    VirtualKeyCode::A | VirtualKeyCode::Left => {
-                        self.is_left_pressed = is_pressed;
-                        true
-                    }
-                    VirtualKeyCode::S | VirtualKeyCode::Down => {
-                        self.is_backward_pressed = is_pressed;
-                        true
-                    }
-                    VirtualKeyCode::D | VirtualKeyCode::Right => {
-                        self.is_right_pressed = is_pressed;
-                        true
-                    }
-                    _ => false,
-                }
-            }
-            _ => false,
-        }
-    }
-
-    fn update_camera(&self, camera: &mut Camera) {
-        use cgmath::InnerSpace;
-        let forward = camera.target - camera.eye;
-        let forward_norm = forward.normalize();
-        let forward_mag = forward.magnitude();
-
-        // Prevents glitching when camera gets too close to the
-        // center of the scene.
-        if self.is_forward_pressed && forward_mag > self.speed {
-            camera.eye += forward_norm * self.speed;
-        }
-        if self.is_backward_pressed {
-            camera.eye -= forward_norm * self.speed;
-        }
-
-        let right = forward_norm.cross(camera.up);
-
-        // Redo radius calc in case the fowrard/backward is pressed.
-        let forward = camera.target - camera.eye;
-        let forward_mag = forward.magnitude();
-
-        if self.is_right_pressed {
-            // Rescale the distance between the target and eye so 
-            // that it doesn't change. The eye therefore still 
-            // lies on the circle made by the target and eye.
-            camera.eye = camera.target - (forward + right * self.speed).normalize() * forward_mag;
-        }
-        if self.is_left_pressed {
-            camera.eye = camera.target - (forward - right * self.speed).normalize() * forward_mag;
-        }
     }
 }
