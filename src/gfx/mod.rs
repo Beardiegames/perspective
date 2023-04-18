4dc58d19db8c6f9ab4f1eb1e162284d58238cb76
Thomas van den Bighelaar <thomas@beardiegames.com>
2023-04-19 01:22:37 +0200

    extreme ways to get futures, maybe try another route

--- src/gfx/mod.rs
+++ src/gfx/mod.rs
@@ -1,11 +1,12 @@
-mod core;
+mod build_core;
+pub mod encoders;
 mod pipeline;
-mod buffers;
 mod bindgroups;
+mod buffers;
 
 use pollster::FutureExt;
 use wgpu::{InstanceDescriptor, BindGroupLayout};
-
+use raw_window_handle::*;
 
 pub struct BufferHandle<'a, T> {
     data: &'a [T],
@@ -22,8 +23,9 @@ pub struct BindgroupHandle {
 }
 
 impl BindgroupHandle {
-    pub fn set_idx(&self) -> &u32 { &self.set_idx }
-    pub fn bond_idx(&self) -> &u32 { &self.bond_idx }
+    pub fn index(&self) -> &u32 { &self.set_idx }
+    // pub fn set_idx(&self) -> &u32 { &self.set_idx }
+    // pub fn bond_idx(&self) -> &u32 { &self.bond_idx }
 }
 
 pub trait PipelineHandle {
@@ -41,11 +43,20 @@ impl PipelineHandle for ComputePipeHandle {
     }
 }
 
+pub struct WindowSettings<'a, W>
+    where W: HasRawWindowHandle + HasRawDisplayHandle,
+{
+    pub window: &'a W,
+    pub width: u32,
+    pub height: u32
+}
+
 pub struct WgpuCore {
     pub instance: wgpu::Instance,
     pub adapter: wgpu::Adapter,
     pub device: wgpu::Device,
     pub queue: wgpu::Queue,
+    pub surface: Option<wgpu::Surface>,
 
     bindgroup_count: u32,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,86 @@
 mod gfx;
+use gfx::encoders::FromBytes;
+
+use winit::{
+	event_loop::EventLoop, 
+	window::WindowBuilder
+};
+
+impl FromBytes for u32 {
+	fn from_bytes(b: &[u8]) -> u32 {
+		let input = &mut b.clone();
+		let (int_bytes, rest) = input.split_at(std::mem::size_of::<u32>());
+	    *input = rest;
+	    u32::from_ne_bytes(int_bytes.try_into().unwrap())
+	}
+}
+
 
 fn main() -> anyhow::Result<()> {
 
+    // create winit application window
+    let event_loop = EventLoop::new();
+    let window = &WindowBuilder::new().build(&event_loop).unwrap();
+
+    // create wgpu handle
+    let mut wgpu_core = gfx::WgpuCore::new(Some(
+        gfx::WindowSettings { window, width: 800, height: 600 } 
+    ))?;
+
+    // log to check if everything is going well
+    println!("{:?}", wgpu_core.adapter.get_info());
+
+    // setup compute data
     let vec = vec![1, 2, 3, 4];
     let data = &vec.as_slice();
     
-    let mut wgpu_core = gfx::WgpuCore::new(None)?;
-    println!("{:?}", wgpu_core.adapter.get_info());
-
+    // setup compute shader handles
     let shader_str = include_str!("shaders/compute_shader.wgsl");
     let compute_pipe = wgpu_core.setup_compute_pipeline("compute_pipe", shader_str, "main");
     let compute_buffers = wgpu_core.create_buffer_handles("compute_buffer", data);
-
     let compute_bindgroup = wgpu_core.setup_bind_group("compute_group", &compute_pipe, &compute_buffers);
 
-    wgpu_core.setup_compute_command_encoder(&compute_pipe, &compute_bindgroup, &compute_buffers);
+    // build compute shader
+    let compute_process = wgpu_core.execute_compute_passes(&compute_pipe, &compute_bindgroup, &compute_buffers);
 
-    Ok(())
+     	// Poll the device in a blocking manner so that our future resolves.
+     // In an actual application, `device.poll(...)` should
+     // be called in an event loop or on another thread.
+     wgpu_core.device.poll(wgpu::Maintain::Wait); 
+
+	match compute_process.state() {
+		std::task::Poll::Ready(r) => {
+			match r {
+				Ok(data) => println!("DATA: {:?}", data),
+				Err(e) => println!("ERROR: {}", e.to_string()),
+			};
+		},
+		std::task::Poll::Pending => {
+			println!("STILL PENDING!!!");
+		}
+	}
+	
+    
+    // // run
+    // event_loop.run(move |event, _, control_flow| match event {
+    //     Event::WindowEvent {
+    //         ref event,
+    //         window_id,
+    //     } if window_id == window.id() => match event {
+    //         WindowEvent::CloseRequested
+    //         | WindowEvent::KeyboardInput {
+    //             input:
+    //                 KeyboardInput {
+    //                     state: ElementState::Pressed,
+    //                     virtual_keycode: Some(VirtualKeyCode::Escape),
+    //                     ..
+    //                 },
+    //             ..
+    //         } => *control_flow = ControlFlow::Exit,
+    //         _ => {}
+    //     },
+    //     _ => {}
+    // });
+
+    Ok(())    
 }
